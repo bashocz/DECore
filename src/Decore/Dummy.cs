3b916db90c1e1cae53937e4addfdab05dc256634
Petr Baroš <pbaros@gmail.com>
2016-08-21 22:31:31 +0200

    added dummy class and test and closed #1

---
+++ src/Decore/Dummy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Decore
+{
+    public class Dummy
+    {
+        public int Summary(params int[] numbers)
+        {
+            return numbers.Sum();
+        }
+    }
+}
